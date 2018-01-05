4534b9afabb6251b676341a14e6b32cb9bd74df2
Anderson Alvarez V <andersonav91@gmail.com>
2018-01-05 13:05:58 -0500

    First page of pdf

--- lib/pdfs/registration_form.rb
+++ lib/pdfs/registration_form.rb
@@ -11,6 +11,7 @@ module Pdfs
       title
       body
       form
+      signatures
     end
 
     def title
@@ -105,6 +106,8 @@ module Pdfs
         text 'If seller’s or buyer’s agent is checked above, the real estate licensee must complete the following section:', size: 10, align: :left, inline_format: true
       end
 
+      block_cursor_y = 0
+
       font("#{Rails.root}/public/fonts/ARIAL.TTF") do
         move_down 10
         cursor_y = cursor
@@ -117,14 +120,16 @@ module Pdfs
 
         line [75, cursor_y - 12], [300, cursor_y - 12]
 
-        line [315, cursor_y - 12], [530, cursor_y - 12]
+        line [315, cursor_y - 12], [540, cursor_y - 12]
 
         move_cursor_to cursor_y
         checkbox(true, 315, cursor_y)
         move_cursor_to cursor_y
         draw_text("Designated Agency", at: [330, cursor - 8], size: 8)
 
-        text_box("The real estate firm or business listed below and all other affiliated agents are also working as the agent of the", size: 9.5, at: [75, cursor - 16], width: 225)
+        block_cursor_y = cursor - 16
+
+        text_box("The real estate firm or business listed below and all other affiliated agents are also working as the agent of the", size: 9.5, at: [75, block_cursor_y], width: 225)
 
       end
 
@@ -135,7 +140,169 @@ module Pdfs
 
         checkbox(true, 150, cursor_y)
         draw_text("Buyer", at: [160, cursor_y - 8], size: 8)
+      end
 
+      font("#{Rails.root}/public/fonts/ARIAL.TTF") do
+        text_box("Only the licensee named herein represents the", size: 9.5, at: [315, block_cursor_y], width: 225)
+      end
+
+      cursor_y = cursor + 18
+
+      font("#{Rails.root}/public/fonts/ARIALB.TTF") do
+        checkbox(true, 315, cursor_y)
+        draw_text("Seller", at: [325, cursor_y - 8], size: 8)
+
+        checkbox(true, 365, cursor_y)
+        draw_text("Buyer", at: [375, cursor_y - 8], size: 8)
+      end
+
+      font("#{Rails.root}/public/fonts/ARIAL.TTF") do
+        text_box("(designated seller agency or ", size: 9.5, at: [410, cursor_y], width: 150)
+        text_box("designated buyer agency). In this situation any other agents affiliated with the firm or business listed below do not represent you and may represent another party in your real estate transaction.", size: 9.5, at: [315, cursor_y - 11], width: 225)
+      end
+    end
+
+    def signatures
+      # First Block ---------------------------------------------------------------------------------
+
+      move_down 50
+
+      font("#{Rails.root}/public/fonts/ARIAL.TTF") do
+        text "By signing below, I, the real estate licensee, acknowledge that this disclosure has been provided to the consumer named herein:", size: 9, align: :left, inline_format: true
+      end
+
+      cursor_y = 0
+
+      font("#{Rails.root}/public/fonts/ARIALI.TTF") do
+        move_down 20
+
+        table([["<color rgb='FFFFFF'>signature</color>"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5}, :position => 0, :width => 155)
+        draw_text("Signature of Real Estate Licensee", at: [5, cursor - 8], size: 7)
+
+        move_up 12
+
+        cursor_y = cursor
+
+        table([["Some Text"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5}, :position => 160, :width => 185)
+        draw_text('Printed Name of Real Estate Licensee          License #', at: [165, cursor - 8], size:  7)
+
+        move_up 12
+
+        table([["#{Time.now.strftime("%Y-%m-%d")}"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5, align: :center}, :position => 470, :width => 70)
+        draw_text('Today’s Date', at: [470, cursor - 8], size:  7)
+      end
+
+      font("#{Rails.root}/public/fonts/ARIALB.TTF") do
+        checkbox(true, 360, cursor_y)
+        draw_text("Broker", at: [370, cursor_y - 8], size: 7)
+
+        checkbox(true, 405, cursor_y)
+        draw_text("Salesperson", at: [415, cursor_y - 8], size: 7)
+      end
+
+      #------------------------------------------------------------------------------------------------
+
+      # Secound Block ---------------------------------------------------------------------------------
+
+      move_down 20
+
+      cursor_y = 0
+
+      font("#{Rails.root}/public/fonts/ARIALI.TTF") do
+        move_down 20
+
+        table([["Some Text"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5}, :position => 0, :width => 155)
+        draw_text("Name Real Estate Brokerage Firm", at: [5, cursor - 8], size: 7)
+
+        move_up 12
+
+        cursor_y = cursor
+
+        table([["Some Text"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5}, :position => 160, :width => 185)
+        draw_text('Brokerage Firm Real Estate License #', at: [165, cursor - 8], size:  7)
+
+        move_up 12
+
+        table([["<color rgb='FFFFFF'>date</color>"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5, align: :center}, :position => 470, :width => 70)
+      end
+
+      #------------------------------------------------------------------------------------------------
+
+      # Third Block ------------------------------------------------------------------------------------
+
+      move_down 20
+
+      cursor_y = 0
+
+      font("#{Rails.root}/public/fonts/ARIALI.TTF") do
+        move_down 20
+
+        table([["<color rgb='FFFFFF'>signature</color>"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5}, :position => 0, :width => 155)
+        draw_text("Signature of Consumer", at: [5, cursor - 8], size: 7)
+
+        move_up 12
+
+        cursor_y = cursor
+
+        table([["Some Text"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5}, :position => 160, :width => 185)
+        draw_text('Printed Name of Consumer', at: [165, cursor - 8], size:  7)
+
+        move_up 12
+
+        table([["#{Time.now.strftime("%Y-%m-%d")}"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5, align: :center}, :position => 470, :width => 70)
+        draw_text('Today’s Date', at: [470, cursor - 8], size:  7)
+      end
+
+      font("#{Rails.root}/public/fonts/ARIALB.TTF") do
+        checkbox(true, 360, cursor_y)
+        draw_text("Buyer", at: [370, cursor_y - 8], size: 7)
+
+        checkbox(true, 405, cursor_y)
+        draw_text("Seller", at: [415, cursor_y - 8], size: 7)
+      end
+
+      #------------------------------------------------------------------------------------------------
+
+      # Fourth Block ------------------------------------------------------------------------------------
+
+      move_down 20
+
+      cursor_y = 0
+
+      font("#{Rails.root}/public/fonts/ARIALI.TTF") do
+        move_down 20
+
+        table([["<color rgb='FFFFFF'>signature</color>"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5}, :position => 0, :width => 155)
+        draw_text("Signature of Consumer", at: [5, cursor - 8], size: 7)
+
+        move_up 12
+
+        cursor_y = cursor
+
+        table([["Some Text"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5}, :position => 160, :width => 185)
+        draw_text('Printed Name of Consumer', at: [165, cursor - 8], size:  7)
+
+        move_up 12
+
+        table([["#{Time.now.strftime("%Y-%m-%d")}"]] , cell_style: {inline_format: true, borders: [:bottom], padding: [2,2,2,2], border_color: "000000", size: 7.5, align: :center}, :position => 470, :width => 70)
+        draw_text('Today’s Date', at: [470, cursor - 8], size:  7)
+      end
+
+      font("#{Rails.root}/public/fonts/ARIALB.TTF") do
+        checkbox(true, 360, cursor_y)
+        draw_text("Buyer", at: [370, cursor_y - 8], size: 7)
+
+        checkbox(true, 405, cursor_y)
+        draw_text("Seller", at: [415, cursor_y - 8], size: 7)
+      end
+
+      #------------------------------------------------------------------------------------------------
+
+      move_down 40
+
+      checkbox(true, 320, cursor - 14)
+      font("#{Rails.root}/public/fonts/ARIALI.TTF") do
+        draw_text("Check here if the consumer declines to sign this notice", at: [330, cursor], size: 9)
       end
     end
 
