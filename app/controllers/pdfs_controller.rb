9469e420f0cb879c7f2f37f012efe1e1b7e9f819
Anderson Alvarez V <andersonav91@gmail.com>
2018-01-08 13:27:50 -0500

    Post validation

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,32 +1,47 @@
 class PdfsController < ApplicationController
+
+  rescue_from(ActionController::UnpermittedParameters) do |pme|
+    render json: { error:  { unknown_parameters: pme.params } },
+           status: :bad_request
+  end
+
   def registration_of_real_estate_and_selling_brokers
-    # new prawn pdf object
-    pdf = Pdfs::RegistrationForm.new(params[:pdf])
-    pdf_name = "#{Time.now.strftime("%Y%m%dT%H%M%S%z_registration_form")}.pdf"
-    pdf_path = "#{Rails.root}/public/pdfs/#{pdf_name}"
+    # params validation
+    pdf_validation = Pdfs::RegistrationFormValidation.new(params[:pdf])
 
-    # create pdf's folder if not exists
-    pdf_folder = "#{Rails.root}/public/pdfs/"
-    Dir.mkdir(pdf_folder) unless File.exists?(pdf_folder)
+    # if valid form
+    if(pdf_validation.valid?)
 
-    # save pdf on disc and send pdf by email if it is not exists
-    unless File.exists?(pdf_path)
-      # save pdf
-      pdf.render_file(pdf_path)
-      # send email
-      PdfsMailer.registration_form(params[:pdf][:realty_send_email], pdf_name, pdf_path).deliver
-      render json: {
-          status: 'ok',
-          message: 'The pdf file is generated and sent to the specified email.',
-          url: ENV["BACKEND_URL"] + "/pdfs/#{pdf_name}"
-      }, status: :ok
+      # new prawn pdf object
+      pdf = Pdfs::RegistrationForm.new(params[:pdf])
+      pdf_name = "#{Time.now.strftime("%Y%m%dT%H%M%S%z_registration_form")}.pdf"
+      pdf_path = "#{Rails.root}/public/pdfs/#{pdf_name}"
+
+      # create pdf's folder if not exists
+      pdf_folder = "#{Rails.root}/public/pdfs/"
+      Dir.mkdir(pdf_folder) unless File.exists?(pdf_folder)
+
+      # save pdf on disc and send pdf by email if it is not exists
+      unless File.exists?(pdf_path)
+        # save pdf
+        pdf.render_file(pdf_path)
+        # send email
+        PdfsMailer.registration_form(params[:pdf][:realty_send_email], pdf_name, pdf_path).deliver
+        render json: {
+            status: 'ok',
+            message: 'The pdf file is generated and sent to the specified email.',
+            url: ENV["BACKEND_URL"] + "/pdfs/#{pdf_name}"
+        }, status: :ok
+      else
+        render json: {
+            status: 'error',
+            message: 'Error generating the pdf, this already exists.'
+        }, status: :conflict
+      end
     else
-      render json: {
-          status: 'error',
-          message: 'Error generating the pdf, this already exists.'
-      }, status: :conflict
+      # invalid form
+      render json: { status: 'error', message: pdf_validation.errors.full_messages.join(', ') }, status: :not_acceptable
     end
-
   end
 
   def offer_to_purchase_real_estate
---
+++ lib/pdfs/registration_form_validation.rb
@@ -0,0 +1,98 @@
+module Pdfs
+  class RegistrationFormValidation
+    include ActiveModel::Validations
+
+    attr_accessor :real_estate_licensee_type,
+                  :is_designated_agency,
+                  :designated_agency_type,
+                  :printed_name_of_real_estate_licensee,
+                  :license_number,
+                  :printed_name_of_real_estate_licensee_type,
+                  :name_real_estate_brokerage_firm,
+                  :brokerage_firm_real_estate_license_number,
+                  :printed_name_of_consumer1,
+                  :printed_name_of_consumer2,
+                  :customer_type1,
+                  :customer_type2,
+                  :printed_name_of_consumer1,
+                  :printed_name_of_consumer2,
+                  :consumer_declines_to_sign_this_notice,
+                  :realty_send_email
+
+    # params validation
+
+    validates :real_estate_licensee_type, presence: true, numericality: true, inclusion: { in: [1, 2, 3] }
+
+    validates :is_designated_agency, presence: true, inclusion: { in: [ true, false ] }
+
+    validates :designated_agency_type, presence: true, numericality: true, inclusion: { in: [1, 2] }
+
+    validates :is_designated_agency, presence: true
+
+    validates :license_number, presence: true, numericality: true
+
+    validates :printed_name_of_real_estate_licensee, presence: true
+
+    validates :printed_name_of_real_estate_licensee_type, presence: true, numericality: true, inclusion: { in: [1, 2] }
+
+    validates :name_real_estate_brokerage_firm, presence: true
+
+    validates :brokerage_firm_real_estate_license_number, presence: true, numericality: true
+
+    validates :printed_name_of_consumer1, presence: true
+
+    validates :printed_name_of_consumer2, presence: true
+
+    validates :customer_type1, presence: true, numericality: true, inclusion: { in: [1, 2] }
+
+    validates :customer_type2, presence: true, numericality: true, inclusion: { in: [1, 2] }
+
+    validates :printed_name_of_consumer1, presence: true
+
+    validates :printed_name_of_consumer2, presence: true
+
+    validates :consumer_declines_to_sign_this_notice, presence: true, inclusion: { in: [ true, false ] }
+
+    validates_format_of :realty_send_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :allow_blank => false
+
+    def initialize(params={})
+      @real_estate_licensee_type = params[:real_estate_licensee_type]
+      @is_designated_agency = params[:is_designated_agency]
+      @designated_agency_type = params[:designated_agency_type]
+      @printed_name_of_real_estate_licensee = params[:printed_name_of_real_estate_licensee]
+      @license_number = params[:license_number]
+      @printed_name_of_real_estate_licensee_type = params[:printed_name_of_real_estate_licensee_type]
+      @name_real_estate_brokerage_firm = params[:name_real_estate_brokerage_firm]
+      @brokerage_firm_real_estate_license_number = params[:brokerage_firm_real_estate_license_number]
+      @printed_name_of_consumer1 = params[:printed_name_of_consumer1]
+      @printed_name_of_consumer2 = params[:printed_name_of_consumer2]
+      @customer_type1 = params[:customer_type1]
+      @customer_type2 = params[:customer_type2]
+      @printed_name_of_consumer1 = params[:printed_name_of_consumer1]
+      @printed_name_of_consumer2 = params[:printed_name_of_consumer2]
+      @consumer_declines_to_sign_this_notice = params[:consumer_declines_to_sign_this_notice]
+      @realty_send_email = params[:realty_send_email]
+
+      # permited params
+      params.permit(
+          :real_estate_licensee_type,
+          :is_designated_agency,
+          :designated_agency_type,
+          :printed_name_of_real_estate_licensee,
+          :license_number,
+          :printed_name_of_real_estate_licensee_type,
+          :name_real_estate_brokerage_firm,
+          :brokerage_firm_real_estate_license_number,
+          :printed_name_of_consumer1,
+          :printed_name_of_consumer2,
+          :customer_type1,
+          :customer_type2,
+          :printed_name_of_consumer1,
+          :printed_name_of_consumer2,
+          :consumer_declines_to_sign_this_notice,
+          :realty_send_email
+      )
+    end
+  end
+end
+
